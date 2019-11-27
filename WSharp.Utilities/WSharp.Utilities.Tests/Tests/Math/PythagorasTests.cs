59144bc3e80e116fe2a3dcd450af1c508d2bd3e9
wim van laer <van.laer.wim@gmail.com>
2019-11-27 22:05:17 +0100

    feature/pythagoras: created static Pythagoras class that implements his theorem

---
+++ WSharp.Utilities/WSharp.Utilities.Tests/Tests/Math/PythagorasTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using WSharp.Utilities.Math;
+
+namespace WSharp.Utilities.Tests.Tests.Math
+{
+    [TestFixture]
+    public class PythagorasTests
+    {
+        [Test]
+        public void SolveHypotenus()
+        {
+            var a = 3;
+            var b = 4;
+            Pythagoras.Solve(a, b)
+                .Should()
+                .Be(5, "3^2 + 4^2 = 5^2");
+
+            a = 8;
+            b = 6;
+            Pythagoras.Solve(a, b)
+                .Should()
+                .Be(10, "8^2 + 6^2 = 10^2");
+        }
+
+        [Test]
+        public void SolveA()
+        {
+            double? a = null;
+            double? b = 6;
+            double? c = 10;
+
+            Pythagoras.Solve(ref a, ref b, ref c)
+                .Should()
+                .Be(8, "8^2 + 6^2 = 10^2");
+
+            a.Should().Be(8, "8^2 + 6^2 = 10^2");
+        }
+
+        [Test]
+        public void SolveB()
+        {
+            double? a = 3;
+            double? b = null;
+            double? c = 5;
+
+            Pythagoras.Solve(ref a, ref b, ref c)
+                .Should()
+                .Be(4, "3^2 + 4^2 = 5^2");
+
+            b.Should().Be(4, "3^2 + 4^2 = 5^2");
+        }
+
+        [Test]
+        public void SolveC()
+        {
+            double? a = 3;
+            double? b = 4;
+            double? c = null;
+
+            Pythagoras.Solve(ref a, ref b, ref c)
+                .Should()
+                .Be(5, "3^2 + 4^2 = 5^2");
+
+            c.Should().Be(5, "3^2 + 4^2 = 5^5");
+        }
+    }
+}
---
+++ WSharp.Utilities/WSharp.Utilities/Math/Pythagoras.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using M = System.Math;
+
+namespace WSharp.Utilities.Math
+{
+    public static class Pythagoras
+    {
+        public static double Solve(double a, double b) => M.Sqrt(a * a + b * b);
+
+        public static double Solve(ref double? a, ref double? b, ref double? c)
+        {
+            if (a == null && b != null && c != null)
+            {
+                a = M.Sqrt(c.Value * c.Value - b.Value * b.Value);
+                return a.Value;
+            }
+            else if (a != null && b == null && c!= null)
+            {
+                b = M.Sqrt(c.Value * c.Value - a.Value * a.Value);
+                return b.Value;
+            }
+            else if (a != null && b != null && c == null)
+            {
+                c = M.Sqrt(a.Value * a.Value + b.Value * b.Value);
+                return c.Value;
+            }
+
+            int nullCount = 0;
+            if (a == null)
+                nullCount++;
+            if (b == null)
+                nullCount++;
+            if (c == null)
+                nullCount++;
+
+            if (nullCount < 1)
+                throw new InvalidOperationException("Cannot calculate Pythagoras theorem with only one argument");
+            throw new InvalidOperationException("Cannot calculate Pythagoras theorem when all variables are known");
+        }
+
+    }
+}
